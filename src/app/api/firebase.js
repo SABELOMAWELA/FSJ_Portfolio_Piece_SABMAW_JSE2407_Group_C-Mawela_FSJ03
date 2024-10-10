b2cc72abf25cb5e091586db9e8c947668f9f9f51
SABELOMAWELA <Mawelas63@gmail.com>
2024-10-10 13:22:07 +0200

    "Added export default to Firebase.js"

--- src/app/api/firebase.js
+++ src/app/api/firebase.js
@@ -1,8 +1,9 @@
+// Import the necessary Firebase modules
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-
+// Firebase configuration using environment variables
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -14,5 +15,10 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
+
+
+export default app;
+
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
